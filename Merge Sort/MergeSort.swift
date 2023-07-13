f0f335947a7ab70769a6e7d1e712fcd42da4240d
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-07-13 20:15:32 +0600

    Updating MergeSort.swift by adding mergeSort() internals

--- Merge Sort/MergeSort.swift
+++ Merge Sort/MergeSort.swift
@@ -1,7 +1,16 @@
 // Merge Sort algorithm implementation in Swift with comments for improved readability
 
-func mergeSort(_ dataList : [Int]){
+func mergeSort(_ dataList : [Int]) -> [Int] {
 
-  // to be updated
+  guard dataList.count > 0 else {
+  return dataList
+  }
+
+  let leftHalf = Array(dataList[0..<dataList.count/2])
+  let rightHalf = Array(dataList[dataList.count/2..<dataList.count])
+
+  return mergeLists(left: mergeSort(leftHalf), right: mergeSort(rightHalf))
+  
+  // mergeLists() to be updated
   
 }
